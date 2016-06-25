bfba77213729df2ebed3ec5e0f62594fc9a27f86
mjuarez <mjuarez@yammer-inc.com>
2016-06-25 13:43:49 -0700

    Added ability to play one mp3 file from the cloud

--- app/src/main/java/com/murki/vinojams/SongListActivity.java
+++ app/src/main/java/com/murki/vinojams/SongListActivity.java
@@ -1,10 +1,13 @@
 package com.murki.vinojams;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,9 +15,13 @@ import android.view.MenuItem;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.IOException;
+
 public class SongListActivity extends AppCompatActivity {
 
     private FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +30,36 @@ public class SongListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                StorageReference storageRef = firebaseStorage.getReferenceFromUrl("gs://project-8042746893150109988.appspot.com/");
-                Snackbar.make(view, storageRef.child("Come Thou fount of Every Blessing.mp3").getBucket(), Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(final View view) {
+                final StorageReference storageRef = firebaseStorage.getReferenceFromUrl("gs://project-8042746893150109988.appspot.com/Come Thou fount of Every Blessing.mp3");
+                try {
+//                    mediaPlayer.setDataSource(storageRef.getDownloadUrl().getResult().toString());
+                    mediaPlayer.setDataSource("https://firebasestorage.googleapis.com/v0/b/project-8042746893150109988.appspot.com/o/Come%20Thou%20fount%20of%20Every%20Blessing.mp3?alt=media&token=988cb528-9d09-400c-a9c5-27145bfab3a2");
+                    mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+                        @Override
+                        public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+                            Snackbar.make(view, "ERROR playing " + storageRef.getName(), Snackbar.LENGTH_LONG).show();
+                            Log.e(this.getClass().getCanonicalName(), "Error preparing.");
+                            return false;
+                        }
+                    });
+                    mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mediaPlayer) {
+                            Snackbar.make(view, "Playing " + storageRef.getName(), Snackbar.LENGTH_LONG).show();
+                            mediaPlayer.start();
+                        }
+                    });
+                    mediaPlayer.prepareAsync();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
