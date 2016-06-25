fb5a20155b306bd17082ec9134342e8fee27d733
mjuarez <mjuarez@yammer-inc.com>
2016-06-25 14:11:25 -0700

    Using the dynamic file store uris to retrieve the file to play

--- app/src/main/java/com/murki/vinojams/SongListActivity.java
+++ app/src/main/java/com/murki/vinojams/SongListActivity.java
@@ -2,7 +2,9 @@ package com.murki.vinojams;
 
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,7 +13,15 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -19,9 +29,13 @@ import java.io.IOException;
 
 public class SongListActivity extends AppCompatActivity {
 
-    private FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+    private final static String CLASSNAME = SongListActivity.class.getCanonicalName();
 
+    private FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private FirebaseAuth.AuthStateListener authStateListener;
     private MediaPlayer mediaPlayer;
+    private Uri pathToSong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +44,51 @@ public class SongListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(CLASSNAME, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(CLASSNAME, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        // authenticate
+        firebaseAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(CLASSNAME, "signInAnonymously:onComplete:" + task.isSuccessful());
+
+                // If sign in fails, display a message to the user. If sign in succeeds
+                // the auth state listener will be notified and logic to handle the
+                // signed in user can be handled in the listener.
+                if (!task.isSuccessful()) {
+                    Log.w(CLASSNAME, "signInAnonymously", task.getException());
+                    Toast.makeText(SongListActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // get file Url
+        final StorageReference storageRef = firebaseStorage.getReferenceFromUrl("gs://project-8042746893150109988.appspot.com/Come Thou fount of Every Blessing.mp3");
+        storageRef.getDownloadUrl().addOnSuccessListener(this, new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Log.d(CLASSNAME, "getDownloadUrl:onSuccess:");
+                pathToSong = uri;
+            }
+        }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(CLASSNAME, "getDownloadUrl:onFailure:", e);
+            }
+        });
+
         mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
@@ -37,15 +96,14 @@ public class SongListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
-                final StorageReference storageRef = firebaseStorage.getReferenceFromUrl("gs://project-8042746893150109988.appspot.com/Come Thou fount of Every Blessing.mp3");
                 try {
-//                    mediaPlayer.setDataSource(storageRef.getDownloadUrl().getResult().toString());
-                    mediaPlayer.setDataSource("https://firebasestorage.googleapis.com/v0/b/project-8042746893150109988.appspot.com/o/Come%20Thou%20fount%20of%20Every%20Blessing.mp3?alt=media&token=988cb528-9d09-400c-a9c5-27145bfab3a2");
+                    mediaPlayer.setDataSource(pathToSong.toString());
+//                    mediaPlayer.setDataSource("https://firebasestorage.googleapis.com/v0/b/project-8042746893150109988.appspot.com/o/Come%20Thou%20fount%20of%20Every%20Blessing.mp3?alt=media&token=988cb528-9d09-400c-a9c5-27145bfab3a2");
                     mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
                         @Override
                         public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
                             Snackbar.make(view, "ERROR playing " + storageRef.getName(), Snackbar.LENGTH_LONG).show();
-                            Log.e(this.getClass().getCanonicalName(), "Error preparing.");
+                            Log.e(CLASSNAME, "Error preparing.");
                             return false;
                         }
                     });
@@ -72,6 +130,12 @@ public class SongListActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -83,5 +147,13 @@ public class SongListActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (authStateListener != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 }
